2f4df6204a0327078170c548486c7c4bcb03082d
batn1ck <you@example.com>
2021-02-14 12:07:52 +0300

    Erato and simple_nums

---
+++ egorikov/task1/erato.py
@@ -0,0 +1,22 @@
+﻿n = int(input('Enter N: '))
+values = [i for i in range(n+1)]
+values.remove(1)
+
+n = len(values)
+i = 1
+
+while i < n:
+	if values[i] != 0:
+		j = values[i]*2
+		step = values[i]
+
+		while j <= n:
+			values[j-1] = 0
+			j += step
+
+	i += 1
+	
+values = set(values)
+values.remove(0)
+print(values)
+input()
---
+++ egorikov/task1/simple_nums.py
@@ -0,0 +1,16 @@
+﻿def find_factors(num):
+	factors = []
+	div_val = 2
+
+	while num != 1:
+		if num % div_val != 0:
+			div_val += 1
+		else:
+			num /= div_val
+			factors.append(div_val)
+	return factors
+
+x = int(input('Enter num: '))
+print(*find_factors(x), sep=' * ', end=' = ')
+print(x)
+input()
